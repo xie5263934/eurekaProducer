81897d06e5a41e3bba278b47a6d7bc67de1b3b7c
sheldonxie <jinrun.xie@56qq.com>
2018-07-05 17:48:25 +0800

    提交

---
+++ src/main/java/com/test/spring/cloud/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.test.spring.cloud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author JINRUN.XIE
+ * @since 2018/7/5
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name){
+        return "hello"+name+", this is first message";
+    }
+}
